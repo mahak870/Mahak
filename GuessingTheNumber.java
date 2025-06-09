6b1280a7cbfa7752d840d31d77d4f27cda095e02
mahak870 <guptamahak730@gmail.com>
2025-06-09 18:51:06 +0530

    Guessing the number

---
+++ GuessingTheNumber.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class GuessingTheNumber {
+  
+    public static void guessingNumberGame(){
+    Scanner sc=new Scanner(System.in);
+    int number=1+(int)(Math.random());
+    int k=5;
+    System.out.println("A number is chosen between 1 to 100");
+    System.out.println("You have" + k + " attempts to guess the correct number.");
+   for(int i=0;i<k;i++){
+    System.out.println("Enter your guess");
+    int guess=sc.nextInt();
+    if(guess==number){
+        System.err.println("Congratulations! You guessed the correct number.");
+        sc.close();
+        return;
+    }
+    else if (guess < number) {
+                System.out.println(
+                    " The number is greater than " + guess);
+            }
+            else {
+                System.out.println(
+                    " The number is less than " + guess);
+            }
+        }
+
+        // If the user runs out of attempts
+        System.out.println(
+            "You've exhausted all attempts. The correct number was: "
+            + number);
+        sc.close();
+    }
+
+    public static void main(String[] args)
+    {
+        guessingNumberGame();
+    }
+}
